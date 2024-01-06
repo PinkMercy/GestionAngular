3b39d871d38b0d93b3cc7bf497821a31a121d4e6
wissem gharbi <55883112+Xkiller92@users.noreply.github.com>
2024-01-06 17:55:04 +0100

    signup page (not finished)

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
 
+export class SignupComponent {
+  login :boolean = true;
+  email :string = "";
+  password:string = "";
+  
+  constructor(private http: HttpClient) { }
+  logStatus(): void {
+    this.login = !this.login;
+  }
+
+  Login(): void{
+    this.http.get<any>('localhost:3000/users');
+  }
+
+  SignIn(): void{
+    this.http.post<any>('localhost:3000/users', { email:this.email, password:this.password});
+  }
 }
